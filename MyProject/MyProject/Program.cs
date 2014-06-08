6aec245c0123d8168b886dc7af712891180d2c6c
Komputer <fdsafdsf@wp.pl>
2014-06-08 13:31:26 +0200

    bye bye test

--- MyProject/MyProject/Program.cs
+++ MyProject/MyProject/Program.cs
@@ -15,6 +15,7 @@ namespace MyProject
             Console.Write("What's your name ? ");
             Console.WriteLine("Hello " + Console.ReadLine());
             Console.ReadKey();
+            // bye bye test
             Console.WriteLine("Bye bye...");
         }
     }
